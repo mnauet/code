a8115bce99e5e2b4b0faa22290781f5e78c8d0a6
mnauet <malik.akram@gmx.de>
2018-07-31 16:26:31 +0200

    2018 07 31 -2

--- pi/pyclient.py
+++
@@ -1,5 +0,0 @@
-import paho.mqtt.client as mqtt
-broker_address ="127.0.0.1"  # "mqtt://localhost"
-client = mqtt.Client("P1")
-client.connect(broker_address)
-client.publish("house/main-light", "OFF")
